90e4c42db94afc146ac7f6df424126e3a92a634f
JavaHunk2020 <javahunk2020@gmail.com>
2021-08-11 06:18:37 +0530

    Base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'movies-ui';
+  //JavaScript and TypeScript we can write together??? yeah
+  title = 32432;
+  message:String="Coming soon , please wait!!";
+
+
+  public clearMessage(){
+    this.message="";
+  }
+
+  public welcomePlease(username:String, password:String) : void {
+      if("jack"==username && "jill"==password){
+        this.message="Hello your username and password are correct!!!!!!!!!!!!!!!!!";
+      }else{
+        this.message="Sorry your username and password are not correct!!!!!!!!!!!!!!!!!";
+      }
+      
+  }
+
 }
